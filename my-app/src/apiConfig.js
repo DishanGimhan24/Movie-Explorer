b78642e702228c41a10a69e416c8c0acbfe430c1
Dishan Gimhan <kdgimhan@gmail.com>
2025-05-12 17:39:02 +0530

    Update apiConfig.js

--- my-app/src/apiConfig.js
+++ my-app/src/apiConfig.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000";
-/* const API_BASE_URL = "https://believable-emotion-production.up.railway.app"; */
+/* const API_BASE_URL = "http://localhost:5000"; */
+const API_BASE_URL = "https://believable-emotion-production.up.railway.app";
 
 export default API_BASE_URL;
